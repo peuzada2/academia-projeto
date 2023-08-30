2af94c9ce5d92204ea5b06fe5e7bb733e9ee6a1e
Pedro Daniel <115499642+peuzada2@users.noreply.github.com>
2023-08-30 17:21:52 -0300

    ATT

--- script.js
+++ script.js
@@ -4,3 +4,29 @@ $(document).ready(function () {
         $(".navegacao").toggleClass("active");
     });
 });
+
+const cards = document.querySelectorAll('.card');
+const prevBtn = document.getElementById('prevBtn');
+const nextBtn = document.getElementById('nextBtn');
+let currentIndex = 0;
+
+function showCard(index) {
+  cards.forEach((card, i) => {
+    card.style.transform = `translateX(${(i - index) * 300}px)`;
+  });
+}
+
+function nextCard() {
+  currentIndex = (currentIndex + 1) % cards.length;
+  showCard(currentIndex);
+}
+
+function prevCard() {
+  currentIndex = (currentIndex - 1 + cards.length) % cards.length;
+  showCard(currentIndex);
+}
+
+prevBtn.addEventListener('click', prevCard);
+nextBtn.addEventListener('click', nextCard);
+
+showCard(currentIndex);
